0830aea5c8fbd352610fc31c7c22d914b77aa4a7
Dillon Allan <dlallan@users.noreply.github.com>
2019-03-22 08:38:02 -0600

    Simple commit to test ssh

---
+++ include/shape.h
@@ -0,0 +1,29 @@
+#ifndef _SHAPE_H_
+#define _SHAPE_H_
+#include <Adruino.h>
+#include <PDQ_GFX.h>
+
+
+// // helper for storing points
+// struct XY_Point {
+//     int8_t x;
+//     int8_t y;
+// }
+
+// // base class for all rendering done in-game.
+// // includes
+// class Shape {
+//     public:
+//     XY_Point pos;
+//     int16_t  colour;
+//     Shape(XY_Point pos, int16_t col) {
+//         this->pos = pos;
+//         this->colour = col;
+//     }
+
+//     void draw(/*need canvas pointer*/);
+
+//     private:
+
+}
+#endif
