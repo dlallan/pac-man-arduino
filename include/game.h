732e31369280696525dad5f8632309d5f70d7ad3
Dillon Allan <dlallan@users.noreply.github.com>
2019-04-06 13:14:22 -0600

    Comment game.[h|cpp]. Moving on to map.[h|cpp]...

--- include/game.h
+++ include/game.h
@@ -1,12 +1,20 @@
+/*---------------------------------------------------------------------------^/
+ | Names: Dillon Allan and Amir Hansen                                       |
+ | ID: 0000000 and 0000001                                                   |
+ | CMPUT 275, Winter 2019                                                    | 
+ | Final Project: Pac Man on Arduino                                         |
+/^---------------------------------------------------------------------------*/
+/*Track state of game*/
 #ifndef game_h
 #define game_h
 #include <Arduino.h>
 
+// Game state stored in instance of Game
 class Game {
     public:
         // start game with given number of lives
         Game(int16_t lives = 3);
-        ~Game();
+        ~Game() {};
 
         // check if out of lives or at max score
         bool isGameOver();
--- src/game.cpp
+++ src/game.cpp
@@ -1,10 +1,15 @@
+/*---------------------------------------------------------------------------^/
+ | Names: Dillon Allan and Amir Hansen                                       |
+ | ID: 0000000 and 0000001                                                   |
+ | CMPUT 275, Winter 2019                                                    | 
+ | Final Project: Pac Man on Arduino                                         |
+/^---------------------------------------------------------------------------*/
+/*Track state of game*/
 #include "game.h"
 
 Game::Game(int16_t lives /*= 3*/) {
     this->lives = lives;
 }
-
-Game::~Game() {}
 
 int16_t Game::getRemainingLives() {
     return lives;
